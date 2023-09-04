18fa7d535679c17abe316ff4672c2321d9e7b595
shonmikl <mikhailegorov676@gmail.com>
2023-09-04 21:45:10 +0200

    IsPrime

--- src/main/java/org/example/_2023_08_28/MainGreedy.java
+++ src/main/java/org/example/_2023_08_28/MainGreedy.java
@@ -8,7 +8,7 @@ public class MainGreedy {
 
     public static void main(String[] args) {
         int[] weights = {3, 4, 5, 8, 9};
-        int[] prices = {1, 6, 4, 7, 6};
+        int[] prices =  {1, 6, 4, 7, 6};
 
         int maxWeight = 13;
 
--- src/main/java/org/example/_2023_09_04/isPrime/Prime.java
+++ src/main/java/org/example/_2023_09_04/isPrime/Prime.java
@@ -2,12 +2,11 @@ package org.example._2023_09_04.isPrime;
 
 public class Prime {
     public static void main(String[] args) {
-        for (int i = 1; i < 500; i++) {
-            if(isPrime(i)) {
-                System.out.println(i + " : " + isPrime(i));
-            }
-        }
+
+        System.out.println("49 : " + isPrime(49));
+
     }
+
     private static boolean isPrime(int number) {
         if (number < 2) {
             return false;
@@ -20,7 +19,7 @@ public class Prime {
         if (number % 3 == 0) {
             return number == 3;
         }
-
+//49
         for (int i = 5; i * i <= number; i += 6) {
             if (number % i == 0 || number % (i + 2) == 0) {
                 return false;
